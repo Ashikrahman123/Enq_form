0f89aa11ad686dee52920e21c62efd52c7cd448d
Mohamed Asik Rahman <40391581+Ashikrahman123@users.noreply.github.com>
2023-10-21 20:23:16 +0530

    Add files via upload

---
+++ process_form.php
@@ -0,0 +1,69 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $mob_no = $_POST["mob_no"];
+    $email = $_POST["email"];
+    $address = $_POST["address"];
+
+    // Database connection details
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "enq_form";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Insert data into the database
+    $sql = "INSERT INTO cform (`name`, `mob_no`, `email`, `address`) VALUES ('$name', '$mob_no', '$email', '$address')";
+
+    if ($conn->query($sql) === TRUE) {
+        // Create a PHPMailer object
+        $mail = new PHPMailer(true);
+
+        try {
+            // Server settings
+            $mail->isSMTP();
+            $mail->Host       = 'smtp.gmail.com';
+            $mail->SMTPAuth   = true;
+            $mail->Username   = 'ashikr142@gmail.com'; // Your Gmail address
+            $mail->Password   = 'xmvjpffixoewnwax'; // Your Gmail password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // or PHPMailer::ENCRYPTION_SSL
+            $mail->Port       = 587; // TCP port to connect to
+
+            // Recipients
+            $mail->setFrom('ashikr142@gmail.com', 'Ashik Rahman');
+            $mail->addAddress($email, $name); // Add a recipient
+
+            // Content
+            $mail->isHTML(true);
+            $mail->Subject = 'Registration Confirmation';
+            $mail->Body    = "Hello $name,<br><br>Thank you for your registration. You are successfully registered with the following details:<br><br>Name: $name<br>Mobile Number: $mob_no<br>Email: $email<br>Address: $address<br><br>Best regards,<br>Your Website Team";
+
+            $mail->send();
+
+            // Redirect user to a thank you page
+            header("Location: thank_you.php");
+            exit();
+        } catch (Exception $e) {
+            echo "Email could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
